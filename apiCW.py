87a839d15982f861b71f515b3538748eec07b895
Dustin Rowland <lee.rowland@gmail.com>
2016-08-19 12:24:37 -0500

    Fixed report output, updated docstrings
    Re-wrote the xlsxwriter workbook output to use new dictionary parseJSON
    data.  Removed all explicit variables for techs, used implicit single
    variable in loop.  Also added/updated docstrings to be PEP257 compliant.

--- apiCW.py
+++ apiCW.py
@@ -7,7 +7,7 @@ Pull data from Connectwise API, generate reports.
 """
 import base64
 import datetime
-import requests  # requires PIP installation
+import requests    # requires PIP installation
 import xlsxwriter  # requires PIP installation
 
 __author__ = 'Dustin Rowland'
@@ -15,23 +15,29 @@ __email__ = 'lee.rowland@gmail.com'
 __copyright__ = '2016, Dustin Rowland'
 __status__ = 'Development'
 
-# Look for ToDo comments for changes/projects list
-
 
 class apiCW():
+    """apiCW class.
+
+    Primary utility class to connect to Connectwise servers and run queries
+    against REST API.  Referenced below in reportCW class.
+    """
+
     def __init__(self):
-        """self.companyName = '<CW API CompanyName>'
+        """apiCW initialization.
+
+        Assigns variables needed in methods.
+        """
+        self.companyName = '<CW API CompanyName>'
         self.pubKey = '<CW API public key>'
-        self.privKey = '<CW API private key>'"""
-        self.companyName = 'protech'
-        self.pubKey = 'NFQIspb9XtiIlpT5'
-        self.privKey = 'WyGglWJQxhWswUAS'
+        self.privKey = '<CW API private key>'
 
     def b64ClientID(self):
-        """
-        Uses the defined companyName, pubKey, and privKey to generate a
-        client_id. Encodes the client_id at utf-8, instead of ASCII, then
-        converts it to base64, then decode as utf-8 to remove byte 'b' prefix
+        r"""b64ClientID method.
+
+        Use the defined companyName, pubKey, and privKey to generate a
+        client_id. Encode the client_id at utf-8, instead of ASCII, then
+        convert it to base64, then decode as utf-8 to remove byte 'b' prefix
         and \n newline.
         """
         client_id = '{}+{}:{}'.format(self.companyName, self.pubKey,
@@ -43,25 +49,32 @@ class apiCW():
         return cid_decode
 
     def formatDate(self, datestamp):
+        """formatDate method.
+
+        Quick method to properly format the datestamp from the REST API into
+        Python formats.
+        """
         return datetime.datetime.strptime(datestamp[0:16], '%Y-%m-%dT%H:%M')
 
-    def queryAPI(self, section, subsection, page):
-        """
+    def queryAPI(self, sect, subsect, page):
+        """queryAPI method.
+
         Query the Connectwise API for a specified request. This will pull down
         a JSON response for parsing later. Uses the b64_clientid function in
         the header variable.
         """
         API_base = ('https://api-na.myconnectwise.net/v4_6_release/'
-                    'apis/3.0/{}'.format(section))
+                    'apis/3.0/{}'.format(sect))
         header = {'Authorization': 'Basic {}'.format(self.b64ClientID())}
         payload = {'pageSize': '100', 'page': page, 'orderBy': 'id desc',
                    'conditions': ''}
-        r = requests.get(API_base + '/' + subsection, params=payload,
+        r = requests.get(API_base + '/' + subsect, params=payload,
                          headers=header)
         return r.json()
 
-    def parseJSON(self, section, subsection, startDate, endDate):
-        """
+    def parseJSON(self, sect, subsect, startDate, endDate):
+        """parseJSON method.
+
         Call queryAPI method to pull dataset out of online system. Will parse
         JSON for data between date ranges, pull out key information.
         """
@@ -71,14 +84,9 @@ class apiCW():
         page = 1
 
         while page < 11:  # ToDo: need to verify 10 pages of tickets is enough
-            for i in self.queryAPI(section, subsection, page):
+            for i in self.queryAPI(sect, subsect, page):
                 # Verify entry timeStart is between startDate and endDate
                 if startDate <= self.formatDate(i['timeStart']) <= endDate:
-                    """ ToDo
-                    This needs to create a nested dictionary of ticket
-                    information based on enteredBy. Then we can pass the
-                    dictionary to the reports for sumation.
-                    """
                     ieB = i['enteredBy']
                     icTI = i['chargeToId']
                     ihB = i['hoursBilled']
@@ -104,37 +112,41 @@ class apiCW():
 
 
 class reportCW(apiCW):
-    def __init__(self, section, subsect, startDate, endDate):
+    """reportCW class.
+
+    Reporting class for Connectwise data.  Uses apiCW class as base to grab
+    and parse JSON data into usable formats.  This is used to generate reports
+    based on already downloaded/parsed data.
+    """
+
+    def __init__(self, sect, subsect, startDate, endDate):
+        """reportCW initialization.
+
+        Assigns variables needed in methods and calls
+        the parseJSON method from apiCW to get the parsed dataset.
+        """
         apiCW.__init__(self)
         self.timestamp = datetime.datetime.now()
         self.now = self.timestamp.strftime('%Y-%m-%d')
-        self.reportData = self.parseJSON(section, subsect, startDate, endDate)
+        self.repData = self.parseJSON(sect, subsect, startDate, endDate)
+        self.startDate = startDate
+        self.endDate = endDate
 
     def reportTicketsPerCompany(self):
-        """
-        .
+        """reportTicketsPerCompany method.
+
+        Look at each company name listed in self.repData, count number of
+        tickets opened over period, then output to Excel file with chart.
         """
         pass
 
     def reportTimePerTech(self):
-        """
-        Look for technician names in JSON data, count number of hours billed,
-        then output to Excel file with chart.
-        """
-        for tech in self.reportData:
-            timeE = 0
-            for ticket in self.reportData[tech]:
-                timeE += float(self.reportData[tech][ticket]['hoursBilled'])
-            self.reportData[tech]['totalTime'] = timeE
-            print(tech, self.reportData[tech]['totalTime'])
+        """reportTimePerTech method.
 
-        print(self.reportData['DRowland'])
-        """ ToDo
-        Once tech time is figured out, re-write report to use the new variable
-        techs to generate report/chart.
+        Look for technician names in self.reportData, count number of hours
+        billed, then output to Excel file with chart.
         """
-
-    '''    with xlsxwriter.Workbook('Report.xlsx') as workbook:
+        with xlsxwriter.Workbook('TimePerTech_Report.xlsx') as workbook:
             workbook.set_properties({
                 'title': 'Time Entries Report',
                 'subject': 'Time Entries Report per Tech',
@@ -153,38 +165,43 @@ class reportCW(apiCW):
                                                   'font_color': '#ffd699',
                                                   'bottom': 1})
 
-            worksheet1 = workbook.add_worksheet('Time Report')
+            worksheet1 = workbook.add_worksheet('TimeReport')
             worksheet1.set_landscape()
             worksheet1.set_paper(1)         # 8x11 paper
             worksheet1.fit_to_pages(1, 1)   # scale down to fit on 1 paper
             worksheet1.set_margins(left=0.7, right=0.7, top=0.5, bottom=0.25)
             worksheet1.set_row(40)
-            worksheet1.merge_range('A1:I1',
+            worksheet1.merge_range('A1:H1',
                                    'Time Entry Report for {} - {}'.format(
-                                                           startDate, endDate),
+                                                           self.startDate,
+                                                           self.endDate),
                                    header_format)
-            worksheet1.set_column(0, 0, 11)
-            worksheet1.write('A2', 'D. Rowland', colhead_format)
-            worksheet1.set_column(1, 1, 11)
-            worksheet1.write('B2', 'R. Koutz', colhead_format)
-            worksheet1.set_column(2, 2, 11)
-            worksheet1.write('C2', 'K. Masoner', colhead_format)
-            worksheet1.set_column(3, 3, 11)
-            worksheet1.write('D2', 'D. Yeager', colhead_format)
 
-            worksheet1.write(2, 0, drTime)
-            worksheet1.write(2, 1, rkTime)
-            worksheet1.write(2, 2, kmTime)
-            worksheet1.write(2, 3, dyTime)
+            row = 1
+            col = 0
+            for tech in self.repData:
+                tTime = 0
 
-    #        chart = workbook.add_chart({'type': 'column'})
-    #        chart.set_title({'name': 'Hours per Tech over Period'})
-    #        chart.add_series({
-    #                            'categories': ['Time Report', 1, 0, 1, 3],
-    #                            'values': ['Time Report', 2, 0, 2, 3],
-    #                            'data_labels': {'value': True}
-    #                        })
-    #        worksheet1.insert_chart('A4', chart)'''
+                for ticket in self.repData[tech]:
+                    tTime += float(self.repData[tech][ticket]['hoursBilled'])
+                self.repData[tech]['totalTime'] = tTime
+
+                worksheet1.write_string(row, col, tech, colhead_format)
+                worksheet1.write_number(row + 1, col, tTime)
+                col += 1
+
+            worksheet1.set_column(0, col + 1, 11)  # expand A-F for header
+
+            chart = workbook.add_chart({'type': 'column'})
+            chart.set_size({'width': 620, 'height': 300})
+            chart.set_title({'name': 'Hours per Tech over Period'})
+            chart.set_legend({'none': True})
+            chart.add_series({
+                                'categories': ['TimeReport', 1, 0, 1, col - 1],
+                                'values': ['TimeReport', 2, 0, 2, col - 1],
+                                'data_labels': {'value': True}
+                            })
+            worksheet1.insert_chart('A4', chart)
 
 
 if __name__ == "__main__":
